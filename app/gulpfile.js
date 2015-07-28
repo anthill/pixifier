0697cf77fca1f9af2ddbd214aed5d4a86bfdfed6
4rzael <maxime.agor@epitech.eu>
2015-07-28 14:07:02 +0000

    compute.js call deleted, initializer now local

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -6,13 +6,7 @@ var gulp = require('gulp');
 
 var server = require('gulp-live-server'); 
 
-var binder = server.new(['./src/compute.js']);
-var initializer = server.new(['./src/init.js']);
-
 gulp.task('init', function () {
+	var initializer = server.new(['./src/init.js']);
     initializer.start();
 });
-
-gulp.task('compute', function() {
-    binder.start();
-})
